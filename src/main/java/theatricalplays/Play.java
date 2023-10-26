b705091a0d7eb34afd426823689b298e23bd2b17
belkacem <alycia.belkacem.etu@univ-lille.fr>
2023-10-26 15:47:53 +0200

    Partie 1 : gérer les sommes en flottant

--- src/main/java/theatricalplays/Play.java
+++ src/main/java/theatricalplays/Play.java
@@ -2,16 +2,13 @@ package theatricalplays;
 
 public class Play {
 
-
   public String name;
   public String type;
   public static final String TRAGEDY = "tragedy";
   public static final String COMEDY = "comedy";
 
-
   public Play(String name, String type) {
     this.name = name;
     this.type = type;
-
   }
 }
--- src/main/java/theatricalplays/StatementPrinter.java
+++ src/main/java/theatricalplays/StatementPrinter.java
@@ -7,7 +7,7 @@ import java.util.Map;
 public class StatementPrinter {
 
   public String print(Invoice invoice, Map<String, Play> plays) {
-    int totalAmount = 0;
+    double totalAmount = 0.0;
     int volumeCredits = 0;
     StringBuffer result = new StringBuffer();
     result.append(String.format("Statement for %s\n", invoice.customer));
@@ -16,40 +16,40 @@ public class StatementPrinter {
 
     for (Performance perf : invoice.performances) {
       Play play = plays.get(perf.playID);
-      int thisAmount = calculateAmount(play,perf);
+      double thisAmount = calculateAmount(play,perf);
       // add volume credits
       volumeCredits += calculateVolumeCredits(play,perf);
 
       // print line for this order
-      result.append(String.format("  %s: %s (%s seats)\n", play.name, currencyFormatter.format(thisAmount / 100), perf.audience));
+      result.append(String.format("  %s: %s (%s seats)\n", play.name, currencyFormatter.format(thisAmount), perf.audience));
       totalAmount += thisAmount;
     }
-    result.append(String.format("Amount owed is %s\n", currencyFormatter.format(totalAmount / 100)));
+    result.append(String.format("Amount owed is %s\n", currencyFormatter.format(totalAmount)));
     result.append(String.format("You earned %s credits\n", volumeCredits));
     return result.toString();
   }
 
-    private int calculateAmount(Play play, Performance perf) {
-        int thisAmount = 0;
-        switch (play.type) {
-            case Play.TRAGEDY:
-                thisAmount = 40000;
-                if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-                break;
-            case Play.COMEDY:
-                thisAmount = 30000;
-                if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience;
-                break;
-            default:
-                throw new Error("Type inconnu : " + play.type);
-        }
-        return thisAmount;
-    }
+    private double calculateAmount(Play play, Performance perf) {
+         double thisAmount = 0.0;
+         switch (play.type) {
+             case Play.TRAGEDY:
+                 thisAmount = 400.0;
+                 if (perf.audience > 30) {
+                     thisAmount += 10.0 * (perf.audience - 30);
+                 }
+                 break;
+             case Play.COMEDY:
+                 thisAmount = 300.0;
+                 if (perf.audience > 20) {
+                     thisAmount += 100.0 + 5.0 * (perf.audience - 20);
+                 }
+                 thisAmount += 3.0 * perf.audience;
+                 break;
+             default:
+                 throw new Error("Unknown type: " + play.type);
+         }
+         return thisAmount;
+     }
 
     private int calculateVolumeCredits(Play play, Performance perf) {
         int volumeCredits = Math.max(perf.audience - 30, 0);
