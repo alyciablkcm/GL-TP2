c81c7dca6eb9bbf6ac7b0bcd8a9f4c09fff6fd8e
MoussiDahbia <moussi95dabi@gmail.com>
2023-10-26 15:23:45 +0200

    Partie 1 : Création des variables statiques pour les types de pièces de théatre

--- src/main/java/theatricalplays/Play.java
+++ src/main/java/theatricalplays/Play.java
@@ -2,11 +2,16 @@ package theatricalplays;
 
 public class Play {
 
+
   public String name;
   public String type;
+  public static final String TRAGEDY = "tragedy";
+  public static final String COMEDY = "comedy";
+
 
   public Play(String name, String type) {
     this.name = name;
     this.type = type;
+
   }
 }
--- src/main/java/theatricalplays/StatementPrinter.java
+++ src/main/java/theatricalplays/StatementPrinter.java
@@ -32,13 +32,13 @@ public class StatementPrinter {
     private int calculateAmount(Play play, Performance perf) {
         int thisAmount = 0;
         switch (play.type) {
-            case "tragedy":
+            case Play.TRAGEDY:
                 thisAmount = 40000;
                 if (perf.audience > 30) {
                     thisAmount += 1000 * (perf.audience - 30);
                 }
                 break;
-            case "comedy":
+            case Play.COMEDY:
                 thisAmount = 30000;
                 if (perf.audience > 20) {
                     thisAmount += 10000 + 500 * (perf.audience - 20);
@@ -54,7 +54,7 @@ public class StatementPrinter {
     private int calculateVolumeCredits(Play play, Performance perf) {
         int volumeCredits = Math.max(perf.audience - 30, 0);
          // add extra credit for every ten comedy attendees
-        if ("comedy".equals(play.type)) volumeCredits += Math.floor(perf.audience / 5);
+        if (Play.COMEDY.equals(play.type)) volumeCredits += Math.floor(perf.audience / 5);
         return volumeCredits;
     }
 
