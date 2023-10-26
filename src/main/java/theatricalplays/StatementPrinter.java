c664678fc5d57a00c9fd87fb2fa43d838407b78a
belkacem <alycia.belkacem.etu@univ-lille.fr>
2023-10-26 14:36:45 +0200

    Partie 1 : Amélioration du code => Extraction de méthodes et ajout de commentaires

--- src/main/java/theatricalplays/StatementPrinter.java
+++ src/main/java/theatricalplays/StatementPrinter.java
@@ -11,43 +11,52 @@ public class StatementPrinter {
     int volumeCredits = 0;
     StringBuffer result = new StringBuffer();
     result.append(String.format("Statement for %s\n", invoice.customer));
-
-    NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
+    // Create a currency formatter to format amounts using the US currency format
+    NumberFormat currencyFormatter  = NumberFormat.getCurrencyInstance(Locale.US);
 
     for (Performance perf : invoice.performances) {
       Play play = plays.get(perf.playID);
-      int thisAmount = 0;
-
-      switch (play.type) {
-        case "tragedy":
-          thisAmount = 40000;
-          if (perf.audience > 30) {
-            thisAmount += 1000 * (perf.audience - 30);
-          }
-          break;
-        case "comedy":
-          thisAmount = 30000;
-          if (perf.audience > 20) {
-            thisAmount += 10000 + 500 * (perf.audience - 20);
-          }
-          thisAmount += 300 * perf.audience;
-          break;
-        default:
-          throw new Error("unknown type: ${play.type}");
-      }
-
+      int thisAmount = calculateAmount(play,perf);
       // add volume credits
-      volumeCredits += Math.max(perf.audience - 30, 0);
-      // add extra credit for every ten comedy attendees
-      if ("comedy".equals(play.type)) volumeCredits += Math.floor(perf.audience / 5);
+      volumeCredits += calculateVolumeCredits(play,perf);
 
       // print line for this order
-      result.append(String.format("  %s: %s (%s seats)\n", play.name, frmt.format(thisAmount / 100), perf.audience));
+      result.append(String.format("  %s: %s (%s seats)\n", play.name, currencyFormatter.format(thisAmount / 100), perf.audience));
       totalAmount += thisAmount;
     }
-    result.append(String.format("Amount owed is %s\n", frmt.format(totalAmount / 100)));
+    result.append(String.format("Amount owed is %s\n", currencyFormatter.format(totalAmount / 100)));
     result.append(String.format("You earned %s credits\n", volumeCredits));
     return result.toString();
   }
+
+    private int calculateAmount(Play play, Performance perf) {
+        int thisAmount = 0;
+        switch (play.type) {
+            case "tragedy":
+                thisAmount = 40000;
+                if (perf.audience > 30) {
+                    thisAmount += 1000 * (perf.audience - 30);
+                }
+                break;
+            case "comedy":
+                thisAmount = 30000;
+                if (perf.audience > 20) {
+                    thisAmount += 10000 + 500 * (perf.audience - 20);
+                }
+                thisAmount += 300 * perf.audience;
+                break;
+            default:
+                throw new Error("Type inconnu : " + play.type);
+        }
+        return thisAmount;
+    }
+
+    private int calculateVolumeCredits(Play play, Performance perf) {
+        int volumeCredits = Math.max(perf.audience - 30, 0);
+         // add extra credit for every ten comedy attendees
+        if ("comedy".equals(play.type)) volumeCredits += Math.floor(perf.audience / 5);
+        return volumeCredits;
+    }
+
 
 }
